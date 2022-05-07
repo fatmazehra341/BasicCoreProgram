09f715279ae47dfd2b983e2d596baffd2372578b
fatmazehra341 <fatmazehra341@gmail.com>
2022-05-08 00:35:34 +0530

    [Fatma] : Flip solution

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//class Power
+class FlipFlop
+{
+	//Function to find count of head and tail
+	public static Tuple<int, int>Count_ht(char s,int N)
+    {
+		//check if initially all the coins are facingtowards head
+		Tuple<int, int> p = Tuple.Create(0, 0);
+		if (s == 'H')
+		{
+			p = Tuple.Create((int)Math.Floor(N / 2.0),
+							 (int)Math.Ceiling(N / 2.0));
+		}
+		else if (s == 'T')
+		{
+			p = Tuple.Create((int)Math.Ceiling(N / 2.0), (int)Math.Ceiling(N / 2.0));
+		}
+			return p;
+        }
+		static void Main()
+        {
+			char C = 'H';
+			int N = 3;
+			Tuple<int, int> p = Count_ht(C,N);
+			Console.WriteLine("Head = " + p.Item1);
+			Console.WriteLine("Tail = " + p.Item2);
+        }
+    }
+	
