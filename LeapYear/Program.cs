73ee63be67cd92ae2a357aef5686131c0032be8a
fatmazehra341 <fatmazehra341@gmail.com>
2022-05-08 00:51:06 +0530

    [Fatma] :Leap year solution

---
+++ LeapYear/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// C# program to check
+// for a leap year
+using System;
+
+class GFG
+{
+
+	static bool checkYear(int year)
+	{
+		// If a year is multiple of 400,
+		// then it is a leap year
+		if (year % 400 == 0)
+			return true;
+
+		// Else If a year is multiple of 100,
+		// then it is not a leap year
+		if (year % 100 == 0)
+			return false;
+
+		// Else If a year is multiple of 4,
+		// then it is a leap year
+		if (year % 4 == 0)
+			return true;
+		return false;
+	}
+
+	// Driver method
+	public static void Main()
+	{
+		int year = 2000;
+		Console.Write(checkYear(year) ? "Leap Year" :
+								"Not a Leap Year");
+	}
+
+}
+
+
